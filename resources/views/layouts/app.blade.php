db4d9c3972d2d22130a68480406cfd07d7590867
Huzaifa <huzaifa.fortunetech@gmail.com>
2026-09-22 23:10:35 +0500

    add Google Tag Manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Trademark USP')</title>
 
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-TSBJ4J7T');
+    </script>
+    <!-- End Google Tag Manager -->
+
+
     <!-- {{-- Fonts --}} -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -183,6 +203,11 @@
     <!-- {{-- JS --}} -->
     <script src="{{ asset('js/scriptt.js') }}?v={{ time() }}"></script>
     <script src="{{ asset('js/script.js') }}?v={{ time() }}"></script>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSBJ4J7T"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     @livewireScripts
     @stack('scripts')
